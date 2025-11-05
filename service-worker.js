0ba98c7737c95105bf046fc1574eb8174824feb2
patrickmonaco <patrick.monaco@yahoo.fr>
2025-11-05 21:48:17 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // === Service Worker Mon Père Lachaise ===
 // Version du cache (change ce numéro à chaque mise à jour)
-const CACHE_NAME = "pwa-cache-v10.84";
+const CACHE_NAME = "pwa-cache-v10.85";
 const ASSETS = [
   "./",
   "./index.html",
@@ -54,6 +54,7 @@ self.addEventListener("message", event => {
     self.skipWaiting();
   }
 });
+
 
 
 
